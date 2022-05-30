fa62d2943a88790dd680a0f7b7bf01062e4796f0
vedika1810 <1905437@kiit.ac.in>
2022-05-30 23:24:05 +0530

    React Route

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,12 +1,14 @@
+import { Link } from "react-router-dom";
+//use "Link to" instead of "a href" tag to let react handle routing instead of server
+
 const Navbar = () => {
     return ( 
         <nav className="navbar">
             <h1>The Dojo Blog</h1>
             <div className="links">
-            <a href="/">Home</a>
-            <a href="/create">New Blog</a>
+            <Link to ="/">Home</Link>
+            <Link to="/create">New</Link>
             </div>
-
         </nav>
      );
 }
