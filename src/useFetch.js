3f47754a1313bcbd508ff8db5fc6a2b0a65d6dd1
vedika1810 <1905437@kiit.ac.in>
2022-05-31 00:00:47 +0530

    useEffect cleanup

--- src/useFetch.js
+++ src/useFetch.js
@@ -5,14 +5,20 @@ const useFetch = (url) => { //3]use url as fetch parameter
     //1] paste all useEffect part and useState (in Home) here
     const [blogs, setBlogs] = useState(null)   
 
+    const abortCont = new AbortController(); //useEffect cleanup
+
     useEffect( ()=>{
-        fetch(url)
+        fetch(url , { signal: abortCont.signal }) //useEffect cleanup
           .then(res => {
               return res.json();
           })
           .then(data => {
               setBlogs(data);
-          });
+          })
+         
+
+         // return () => abortCont.abort(); useEffect cleanup
+
       },[url] );
 
       return {blogs}; //2] return values so that u can get in Home.js
