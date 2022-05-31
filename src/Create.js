c7b14b4f0abffd70ccc7534b4b363ff05a0c0b43
vedika1810 <1905437@kiit.ac.in>
2022-05-31 18:04:31 +0530

    Creating form and Submitting

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,61 @@
+import { useState } from "react";
+
 const Create = () => {
+
+    const[title,setTitle]=useState('');
+    const[body,setBody]=useState('');
+    const[author,setAuthor]=useState('mario');
+
+    const[loading,setloading] =useState(false);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const blog = { title, body, author };
+
+        setloading(true);
+
+        //sending data to json serever => fetch with two args
+         fetch('http://localhost:8000/blogs',{
+             method:'POST',
+             headers: { "Content-Type": "application/json" },
+             body: JSON.stringify(blog)
+         }).then( () => {
+             console.log('Submitted');
+             setloading(false);
+         })
+    }
+
     return ( 
         <div className="create">
             <h2>Add a new Blog</h2>
+            {/* submitting a form (POST REQUEST TO JSON SERVER) */}
+            <form onSubmit={handleSubmit}> 
+                <label>Blog title:</label>
+                <input
+                type="text"
+                value={title}
+                onChange = { (e)=>{setTitle(e.target.value)}}
+                required
+                >
+                </input>
+
+                <label>Blog Body:</label>
+                <textarea 
+                required
+                value={body}
+                onChange = { (e)=>{setBody(e.target.value)}}>
+                </textarea>
+
+                <label>Author:</label>
+                <select
+                value={author}
+                onChange = { (e) => {setAuthor(e.target.value) }}>
+                    <option value="mario">Mario</option>
+                    <option value="luigi">Luigi</option>
+                </select>
+
+                {loading ?<button disabled>Adding Blog</button> :<button>Add Blog</button>}
+            </form>
         </div>
      );
 }
