f9efde925aef22d07023fb8030872a46ab343fed
vedika1810 <1905437@kiit.ac.in>
2022-05-31 18:37:38 +0530

    Deleting Blogs

--- src/BlogsDetail.js
+++ src/BlogsDetail.js
@@ -1,3 +1,4 @@
+import { useHistory } from "react-router-dom";
 import { useParams } from "react-router-dom";
 import useFetch from "./useFetch";
 
@@ -6,6 +7,17 @@ const BlogDetails = () => {
     const {id} = useParams(); //get parameter variable from route url 
     const {blogs} = useFetch('http://localhost:8000/blogs/'+id); // don't froget blogs/
 
+    const history = useHistory();
+
+    const handleClick = () => {
+      fetch('http://localhost:8000/blogs/' + blogs.id, {
+        method: 'DELETE'
+      }).then(() => {
+        history.push('/');
+      }) 
+    }
+
+
     return ( 
         <div className="blog-details">
          { blogs && (
@@ -13,6 +25,7 @@ const BlogDetails = () => {
           <h2>{ blogs.title}</h2>
           <p>Written by { blogs.author }</p>
           <div>{ blogs.body }</div>
+          <button onClick={handleClick}>delete</button>
         </article>
       )}
       
